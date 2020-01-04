4352b83bd72ed77ad3ead0ed247677ac85778cd4
Ramon Oliveira <41554497+RamonPortuga@users.noreply.github.com>
2020-01-04 19:41:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Universinagem</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.css" crossorigin="anonymous">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="carousel.css" rel="stylesheet">
+
+    <!-- Estilos customizados para o site -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Biblioteca de Ícones -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #141D3C; font-family: Comic-book">
+        <a class="navbar-brand" href="#"><img src="logo.png" style="height: 50px; weight: 50px;"> Universinagem</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(atual)</span></a>
+            </li>
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="servicos.php">Serviços</a>
+            </li>
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="servicos.php#materiais">Materiais</a>
+            </li>
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="portfolio_alternativo.php">Portfolio</a>
+            </li>
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="orcamento.php">Orçamento</a>
+            </li>
+            <li class="nav-item active" style="margin-left: 10px;">
+              <a class="nav-link" href="index.php#contatos">Contatos</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main" style="font-family: Comic-book;">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" >
+          <div class="carousel-item active">
+            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Manchete</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Outra manchete</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>Outra manchete, pra garantir</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Voltar</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Avançar</span>
+        </a>
+      </div>
+
+
+
+      <!-- Mensagens de marketing e outras featurezinhas
+      ================================================== -->
+      <!-- Envolve o resto da página em outro container, para centralizar todo o conteúdo. -->
+
+      <div class="container marketing">
+
+        <!-- Três colunas de texto, abaixo do carousel -->
+        <div class="row" style="font-family: Comic-book;">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Impressão 3D</h2>
+            <p>Também conhecida como fabricação aditiva, é o processo pelo qual objetos físicos são criados pela deposição de materiais em camadas, com base em um modelo digital. Todos os processos de impressão 3D requerem o trabalho conjunto de software, hardware e materiais.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Usinagem</h2>
+            <p>Router CNC é um sistema de controle de máquina, realizado através de um computador, e utilizado em tornos e centros de usinagem. Tem como principais aplicações, trabalhos com madeira, plásticos, borrachas, metais não ferrosos, espumas, entre outros.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Título</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+        <!-- COMEÇAM AS MENCIONADAS FEATUREZINHAS xD -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette" style="font-family: Comic-book;">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Nossa Ideia <span class="text-muted">Supreendente, não?!</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette" style="font-family: Comic-book;">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette" style="font-family: Comic-book;">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">E, por último, essa aqui. <span class="text-muted">Xeque-mate!</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /FIM DAS FEATUREZINHAS *-* -->
+
+      </div><!-- /.container -->
+
+      <div class="whatsapp-fixo" style="position:fixed; z-index:999; bottom:10px; right:30px; height: 150px;">
+        <?php
+            $var = mt_rand (0, 2);
+            if ($var == 1) {
+              echo '<a href="https://api.whatsapp.com/send?phone=5521983503996">';
+            }
+            else if ($var == 2) {
+              echo '<a href="https://api.whatsapp.com/send?phone=5521985861986">';
+            }
+         ?>
+  				<img src="whatsapp_icon.png" width="64px;" height="64px;" alt="Fale Conosco pelo WhatsApp"/>
+  			</a>
+  		</div>
+
+      <!-- FOOTER -->
+      <footer class="container" style="font-family: Comic-book;" id = "contatos">
+        <p class="float-right"><a href="#">Voltar ao topo</a></p>
+        <div class="container-fluid">
+					<div class="row">
+				    <div class=" col-md-6" style="margin-top: 4.5%;margin-left:7.5%;">
+				      <h5 style="color: #000000;">Contatos</h5>
+							<h7 style="color: #000000;"> <b>Telefone:</b><br></h7>
+												 <h7 style="margin-left: 10%; color: #000000;">21 983503996<br></h7>
+												 <h7 style="margin-left: 10%; color: #000000;">21 985861986<br></h7>
+				    </div>
+						<div class=" col-md-5" style="margin-top: 4.5%;">
+				      <h5 style="color: #000000">Redes Sociais</h5>
+							<td class="text-center">
+							<a href="https://www.instagram.com/universinagem/?hl=pt"><i class="fab fa-instagram" style="font-size: 35px;"></i></a>
+							<a href="https://www.facebook.com/pg/universinagem/posts/"><i class="fab fa-facebook-square" style="font-size: 35px; margin-left: 5px;"></i></a>
+							<a href="https://www.youtube.com/channel/UCrRQQQIAUdB85VADbDta_MA/featured"><i class="fab fa-youtube" style="font-size: 35px; margin-left: 5px;"></i></a>
+							</td>
+				    </div>
+				  </div>
+					<div class="row">
+						<div class="col-md-5" style="margin-left:7.5%;">
+							<br><h7 style="color: #000000; color: #000000;"><b>Email:</b></h7><br>
+									 <h7 style="margin-left: 10%; color: #000000;"> Contato@universinagem.com.br </h7><br>
+						</div>
+						<div class="col-md-6">
+							<br><h7 style="color: #000000; color: #000000;"><b>Endereço:</b></h7><br>
+									 <h7 style="margin-left: 10%; color: #000000;">Estrada do Galeão, 2760 - bloco B 109 - Portuguesa</h7><br>
+						</div>
+					</div>
+					<div class="row" style="margin-top: 3%;">
+						<h7 style="margin-left: 10%; color: #757575;"> Universinagem © 2019 | Todos os direitos reservados | CNPJ: 35.391.603/0001-46</h7>
+					</div>
+				</div>
+      </footer>
+    </main>
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Só faz o nossos placeholders de imagens funcionarem. Não precisar copiar a próxima linha! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+  </body>
+</html>
